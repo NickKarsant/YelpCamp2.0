a69369a4ff8a46de843eab9b0ff86d14b0bb56d3
Nicholas Karsant <nkarsant@gmail.com>
2021-01-11 18:15:47 -0800

    user form validations

--- app.js
+++ app.js
@@ -1,128 +1,125 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const ejsMate = require('ejs-mate');
-const mongoose = require('mongoose');
-const path = require('path');
+const ejsMate = require("ejs-mate");
+const mongoose = require("mongoose");
+const path = require("path");
 const flash = require("connect-flash");
-const Campground = require('./models/campground');
-const User = require('./models/user');
-const Comment = require('./models/comment');
+const Campground = require("./models/campground");
+const User = require("./models/user");
+const Comment = require("./models/comment");
 const bodyParser = require("body-parser");
-const methodOverride = require('method-override');
+const methodOverride = require("method-override");
 
-
-mongoose.connect("mongodb://localhost:27017/yelpcamp", {
-  useUnifiedTopology: true,
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false
-})
-.then(() => {
-  console.log("YelpCamp database connected")
-})
-.catch(err => {
-  console.log("OH NO ERROR!")
-  console.log(err)
-});
+mongoose
+  .connect("mongodb://localhost:27017/yelpcamp", {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => {
+    console.log("YelpCamp database connected");
+  })
+  .catch(err => {
+    console.log("OH NO ERROR!");
+    console.log(err);
+  });
 
 // App Configuration
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-app.engine('ejs', ejsMate)
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.engine("ejs", ejsMate);
 
 // App Tools
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(methodOverride('_method'));
+app.use(methodOverride("_method"));
 
-
-
-// Routes
-app.use('/yelpcamp/campgrounds', (req,res, next)=>{
-  req.requestTime = Date.now()
+app.use("/yelpcamp/campgrounds", (req, res, next) => {
+  req.requestTime = Date.now();
 
   next();
-})
+});
 
 // Widget app goes here
-app.get('/', (req, res) => {
-  res.send('React app goes here');
+app.get("/", (req, res) => {
+  res.send("React app goes here");
 });
 
 // landing page
-app.get('/yelpcamp', (req, res) => {
-  res.render('landing');
+app.get("/yelpcamp", (req, res) => {
+  res.render("landing");
 });
 
 // show all campgrounds on homepage
-app.get('/yelpcamp/campgrounds', async (req, res) => {
+app.get("/yelpcamp/campgrounds", async (req, res) => {
   const allCamps = await Campground.find({});
-  res.render('campgrounds/index', { allCamps });
+  res.render("campgrounds/index", { allCamps });
 });
 
 // add campground form page
-app.get('/yelpcamp/campgrounds/new', (req, res) => {
-  res.render('campgrounds/new');
+app.get("/yelpcamp/campgrounds/new", (req, res) => {
+  res.render("campgrounds/new");
 });
 
-// add campground to database route
-app.post('/yelpcamp/campgrounds', async (req, res) => {
-  console.log(`Date: ${req.requestTime}`);
-  const campground = new Campground(req.body.campground)
+// ADD  campground to database route
+app.post("/yelpcamp/campgrounds", async (req, res) => {
+  // console.log(`Date: ${req.requestTime}`);
+
+  const campground = new Campground(req.body.campground);
   await campground.save();
-  res.redirect(`/yelpcamp/campgrounds/${campground._id}`)
+  res.redirect(`/yelpcamp/campgrounds/${campground._id}`);
 });
 
-// show specific campground page
-app.get('/yelpcamp/campgrounds/:id', async (req, res) => {
+// SHOW single campground page
+app.get("/yelpcamp/campgrounds/:id", async (req, res) => {
   const campground = await Campground.findById(req.params.id);
   res.render("campgrounds/show", { campground });
 });
 
 // show edit page
-app.get('/yelpcamp/campgrounds/:id/edit', async (req, res) => {
+app.get("/yelpcamp/campgrounds/:id/edit", async (req, res) => {
   const campground = await Campground.findById(req.params.id);
   res.render("campgrounds/edit", { campground });
 });
 
-// update database with edits
-app.put('/yelpcamp/campgrounds/:id', async (req, res) => {
+// UPDATE database with edits
+app.put("/yelpcamp/campgrounds/:id", async (req, res) => {
   const { id } = req.params;
-  const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
-  res.redirect(`/yelpcamp/campgrounds/${campground._id}`)
+  const campground = await Campground.findByIdAndUpdate(id, {
+    ...req.body.campground
+  });
+  res.redirect(`/yelpcamp/campgrounds/${campground._id}`);
 });
 
-// delete
-app.delete('/yelpcamp/campgrounds/:id', async (req,res) => {
+// DELETE
+app.delete("/yelpcamp/campgrounds/:id", async (req, res) => {
   const { id } = req.params;
-  await Campground.findByIdAndDelete(id)
-  res.redirect('/yelpcamp/campgrounds');
+  await Campground.findByIdAndDelete(id);
+  res.redirect("/yelpcamp/campgrounds");
 });
 
-
-
-
-
-app.get('/yelpcamp/login', (req, res) => {
+app.get("/yelpcamp/login", (req, res) => {
   res.render("login");
 });
 
-app.get('/yelpcamp/register', (req, res) => {
+app.get("/yelpcamp/register", (req, res) => {
   res.render("register");
 });
 
+// 404
+app.use((req, res, next) => {
+  res.status(404).render("404");
+});
 
-
-
-
-
-
-app.use((req,res, next)=>{
-  res.status(404).render("404")
-})
-
+app.use(function(err, req, res, next) {
+  console.log("*****ERROR*****");
+  console.log(err);
+  next(err);
+  next();
+});
 
 app.listen(3000, () => {
-  console.log('on port 3000');
+  console.log("on port 3000");
 });
