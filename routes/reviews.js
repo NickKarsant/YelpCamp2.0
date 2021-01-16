85d3a2c75a4296a531764c0e7834479ad3b40f14
Nicholas Karsant <nkarsant@gmail.com>
2021-01-16 13:06:56 -0800

    update

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,42 +1,46 @@
 const express = require("express");
-const router= express.Router( {mergeParams: true} );
+const router = express.Router({ mergeParams: true });
 const ExpressError = require("../utilities/ExpressError");
 const catchAsync = require("../utilities/catchAsync");
 const Campground = require("../models/campground");
 const Review = require("../models/review");
-const { reviewsSchema } = require('../validationSchemas');
+const { reviewsSchema } = require("../validationSchemas");
 
-
-const validateReview = (req, res,next) => {
+const validateReview = (req, res, next) => {
   const { error } = reviewsSchema.validate(req.body);
   if (error) {
-    console.log(error)
-    const msg = error.details.map(el => el.message). join(',')
-    throw new ExpressError(msg, 400)
+    console.log(error);
+    const msg = error.details.map(el => el.message).join(",");
+    throw new ExpressError(msg, 400);
   } else {
     next();
   }
-}
+};
 
 // Review Create
-router.post("/", validateReview, catchAsync(async(req,res) =>{
-  const campground = await Campground.findById(req.params.id);
-  const review = new Review(req.body.review);
-  console.log(req.params)
-  campground.reviews.push(review);
-  await review.save();
-  await campground.save();
-  res.redirect(`/yelpcamp/campgrounds/${campground._id}`);
-}));
+router.post(
+  "/",
+  validateReview,
+  catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review);
+    console.log(req.params);
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/yelpcamp/campgrounds/${campground._id}`);
+  })
+);
 
 // Review Delete
-router.delete('/:reviewId', catchAsync(async (req, res) => {
-  const { id, reviewId } = req.params;
-  await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-  await Review.findByIdAndDelete(reviewId);
-  res.redirect(`/yelpcamp/campgrounds/${id}`);
-}));
-
-
+router.delete(
+  "/:reviewId",
+  catchAsync(async (req, res) => {
+    const { id, reviewId } = req.params;
+    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/yelpcamp/campgrounds/${id}`);
+  })
+);
 
 module.exports = router;
