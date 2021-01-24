6f3d0cf4cbf802ec431a44b06b991f6b98a13d59
Nicholas Karsant <nkarsant@gmail.com>
2021-01-23 20:22:00 -0800

    add geolocation coordinates to map for show page

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,87 +1,96 @@
-const Campground = require('../models/campground');
+const Campground = require("../models/campground");
 const { cloudinary } = require("../cloudinary");
 const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
 const mapBoxToken = process.env.MAPBOX_TOKEN;
 const geocoder = mbxGeocoding({ accessToken: mapBoxToken });
 
-
 module.exports.index = async (req, res) => {
-    const allCamps = await Campground.find({});
-    res.render('campgrounds/index', { allCamps });
-}
+  const allCamps = await Campground.find({});
+  res.render("campgrounds/index", { allCamps });
+};
 
 module.exports.renderNewForm = (req, res) => {
-    res.render('campgrounds/new');
-}
+  res.render("campgrounds/new");
+};
 
 module.exports.createCampground = async (req, res, next) => {
-  
-    const geoData = await geocoder.forwardGeocode({
-        query: req.body.campground.location,
-        limit: 1
-    }).send()
-    const campground = new Campground(req.body.campground);
-    campground.geolocation = geoData.body.features[0].geometry
-    campground.images = req.files.map(file => ({url: file.path, filename: file.filename }))
-    campground.author = req.user._id;
-    await campground.save();
-    req.flash('success', 'Successfully made a new campground!');
-    res.redirect(`/yelpcamp/campgrounds/${campground._id}`)
-}
+  const geoData = await geocoder
+    .forwardGeocode({
+      query: req.body.campground.location,
+      limit: 1
+    })
+    .send();
+  const campground = new Campground(req.body.campground);
+  campground.geolocation = geoData.body.features[0].geometry;
+  console.log(campground.geolocation);
+  campground.images = req.files.map(file => ({
+    url: file.path,
+    filename: file.filename
+  }));
+  campground.author = req.user._id;
+  await campground.save();
+  req.flash("success", "Successfully made a new campground!");
+  res.redirect(`/yelpcamp/campgrounds/${campground._id}`);
+};
 
-module.exports.showCampground = async (req, res,) => {
-    const campground = await Campground.findById(req.params.id).populate({
-        path: 'reviews',
-        populate: {
-            path: 'author'
-        }
-    }).populate('author');
-    if (!campground) {
-        req.flash('error', 'Cannot find that campground!');
-        return res.redirect('/yelpcamp/campgrounds');
-    }
-    res.render('campgrounds/show', { campground });
-}
+module.exports.showCampground = async (req, res) => {
+  const campground = await Campground.findById(req.params.id)
+    .populate({
+      path: "reviews",
+      populate: {
+        path: "author"
+      }
+    })
+    .populate("author");
+  if (!campground) {
+    req.flash("error", "Cannot find that campground!");
+    return res.redirect("/yelpcamp/campgrounds");
+  }
+  res.render("campgrounds/show", { campground });
+};
 
 module.exports.renderEditForm = async (req, res) => {
-    const { id } = req.params;
-    const campground = await Campground.findById(id)
-    if (!campground) {
-        req.flash('error', 'Cannot find that campground!');
-        return res.redirect('/yelpcamp/campgrounds');
-    }
-    res.render('campgrounds/edit', { campground });
-}
+  const { id } = req.params;
+  const campground = await Campground.findById(id);
+  if (!campground) {
+    req.flash("error", "Cannot find that campground!");
+    return res.redirect("/yelpcamp/campgrounds");
+  }
+  res.render("campgrounds/edit", { campground });
+};
 
 module.exports.updateCampground = async (req, res) => {
-    const { id } = req.params;
-    console.log("controller req.body: " + req.body);
-    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
-    const imgs = req.files.map(f => ({ url: f.path, filename: f.filename }));
-    campground.images.push(...imgs);
-    await campground.save();
-    if (req.body.deleteImages) {
-        for (let filename of req.body.deleteImages) {
-            await cloudinary.uploader.destroy(filename);
-        }
-        await campground.updateOne({ $pull: { images: { filename: { $in: req.body.deleteImages } } } })
+  const { id } = req.params;
+  console.log("controller req.body: " + req.body);
+  const campground = await Campground.findByIdAndUpdate(id, {
+    ...req.body.campground
+  });
+  const imgs = req.files.map(f => ({ url: f.path, filename: f.filename }));
+  campground.images.push(...imgs);
+  await campground.save();
+  if (req.body.deleteImages) {
+    for (let filename of req.body.deleteImages) {
+      await cloudinary.uploader.destroy(filename);
     }
-    req.flash('success', 'Successfully updated campground!');
-    res.redirect(`/yelpcamp/campgrounds/${campground._id}`)
-}
+    await campground.updateOne({
+      $pull: { images: { filename: { $in: req.body.deleteImages } } }
+    });
+  }
+  req.flash("success", "Successfully updated campground!");
+  res.redirect(`/yelpcamp/campgrounds/${campground._id}`);
+};
 
 module.exports.deleteCampground = async (req, res) => {
-    const { id } = req.params;
-    await Campground.findByIdAndDelete(id);
-    req.flash('success', 'Successfully deleted campground')
-    res.redirect('/yelpcamp/campgrounds');
-}
-
+  const { id } = req.params;
+  await Campground.findByIdAndDelete(id);
+  req.flash("success", "Successfully deleted campground");
+  res.redirect("/yelpcamp/campgrounds");
+};
 
 module.exports.editImages = async (req, res) => {
-    console.dir(req.files);
-    const campground = req.body.campground;
-    console.log(campground);
-    // const addedImages = req.files.map(file => ({url: file.path, filename: file.filename }))
-    // campground.images.push(addedImages)
-}
+  console.dir(req.files);
+  const campground = req.body.campground;
+  console.log(campground);
+  // const addedImages = req.files.map(file => ({url: file.path, filename: file.filename }))
+  // campground.images.push(addedImages)
+};
--- public/javascript/showPageMap.js
+++ public/javascript/showPageMap.js
@@ -1,7 +1,12 @@
 mapboxgl.accessToken = '<%-process.env.MAPBOX_TOKEN%>';
+
 var map = new mapboxgl.Map({
 container: 'map',
 style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
-center: [-74.5, 40], // starting position [lng, lat]
+center: campground.geometry.coordinates, // starting position [lng, lat]
 zoom: 9 // starting zoom
 });
+
+var marker = new mapboxgl.Marker()
+    .setLngLat(campground.geometry.coordinates)
+    .addTo(map); // add the marker to the map
