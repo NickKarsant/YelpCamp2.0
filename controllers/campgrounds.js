5dc5c67609eb338637d2552e0142a849e22bb0e8
Nicholas Karsant <nkarsant@gmail.com>
2021-01-23 18:22:12 -0800

    add querying  geolocator with location string

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,10 +1,13 @@
 const Campground = require('../models/campground');
 const { cloudinary } = require("../cloudinary");
+const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
+const mapBoxToken = process.env.MAPBOX_TOKEN;
+const geocoder = mbxGeocoding({ accessToken: mapBoxToken });
 
 
 module.exports.index = async (req, res) => {
     const allCamps = await Campground.find({});
-    res.render('campgrounds/index', { allCamps })
+    res.render('campgrounds/index', { allCamps });
 }
 
 module.exports.renderNewForm = (req, res) => {
@@ -12,11 +15,16 @@ module.exports.renderNewForm = (req, res) => {
 }
 
 module.exports.createCampground = async (req, res, next) => {
+  
+    const geoData = await geocoder.forwardGeocode({
+        query: req.body.campground.location,
+        limit: 1
+    }).send()
     const campground = new Campground(req.body.campground);
+    campground.geolocation = geoData.body.features[0].geometry
     campground.images = req.files.map(file => ({url: file.path, filename: file.filename }))
     campground.author = req.user._id;
     await campground.save();
-    console.log(campground.images);
     req.flash('success', 'Successfully made a new campground!');
     res.redirect(`/yelpcamp/campgrounds/${campground._id}`)
 }
